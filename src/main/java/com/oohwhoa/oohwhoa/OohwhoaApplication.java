e99b424524be3ddbf051975715b16e097c45e843
himdol <ihimchan746@gmail.com>
2023-01-28 19:18:07 +0900

    개발 시작

---
+++ src/main/java/com/oohwhoa/oohwhoa/OohwhoaApplication.java
@@ -0,0 +1,13 @@
+package com.oohwhoa.oohwhoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OohwhoaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OohwhoaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/oohwhoa/oohwhoa/OohwhoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oohwhoa.oohwhoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OohwhoaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
