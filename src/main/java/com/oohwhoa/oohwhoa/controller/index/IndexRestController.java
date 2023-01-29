2a7a14e0e130b94b1c85d3b283237432196e50a7
himdol <ihimchan746@gmail.com>
2023-01-29 15:50:51 +0900

    index page 공공데이터 api 구현

---
+++ src/main/java/com/oohwhoa/oohwhoa/controller/index/IndexRestController.java
@@ -0,0 +1,24 @@
+package com.oohwhoa.oohwhoa.controller.index;
+
+import com.oohwhoa.oohwhoa.service.index.IndexService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController()
+public class IndexRestController {
+
+  private IndexService indexService;
+
+  public IndexRestController(IndexService indexService) {
+    this.indexService = indexService;
+  }
+
+  @GetMapping("/all-data")
+  public void getAllData() throws IOException {
+    indexService.getAllData();
+  }
+
+}
