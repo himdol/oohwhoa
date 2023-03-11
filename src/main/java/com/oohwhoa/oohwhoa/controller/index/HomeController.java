f68ee4700d9e80ff578453e0940d9ad45c0fcec4
himdol <ihimchan746@gmail.com>
2023-03-11 14:53:07 +0900

    index page 적용

--- src/main/java/com/oohwhoa/oohwhoa/controller/index/HomeController.java
+++ src/main/java/com/oohwhoa/oohwhoa/controller/index/HomeController.java
@@ -3,21 +3,17 @@ package com.oohwhoa.oohwhoa.controller.index;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller("/")
 public class HomeController {
 
-//  public String showIndexPage() {
-//    return "/";
-//  }
-    @GetMapping(value = "/home")
-    public String goHome(HttpServletRequest request) {
-      return "contents/home";
-    }
-    @GetMapping(value = "/home2")
-    public String goHome2(HttpServletRequest request) {
-      return "contents/home2";
-    }
+  @GetMapping(value = "/")
+  public String goHome(HttpServletRequest request) {
+    return "index";
+  }
+
+  @GetMapping(value = "/home2")
+  public String goHome2(HttpServletRequest request) {
+    return "contents/home2";
+  }
 }
