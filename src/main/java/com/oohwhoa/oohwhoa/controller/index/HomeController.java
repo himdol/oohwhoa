ee8aa2d3007f686b314d7261fc388c1fe7ba5ff0
himdol <ihimchan746@gmail.com>
2023-03-13 10:35:10 +0900

    java version upgrade 로 인한 설정

--- src/main/java/com/oohwhoa/oohwhoa/controller/index/HomeController.java
+++ src/main/java/com/oohwhoa/oohwhoa/controller/index/HomeController.java
@@ -1,8 +1,9 @@
 package com.oohwhoa.oohwhoa.controller.index;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("/")
 public class HomeController {
